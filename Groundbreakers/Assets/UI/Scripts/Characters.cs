bf82fb347f4ed8aff0bc36ba888381dc35239727
Yuming <lionpkqq@gmail.com>
2019-01-14 20:56:33 -0800

    UI Update
    Added the placeholders for different types of UI (Deploy, Status, Characters, Choices).

---
+++ Groundbreakers/Assets/UI/Scripts/Characters.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Characters : MonoBehaviour
+{
+    public GameObject ui;
+
+    public void Toggle()
+    {
+        ui.SetActive(!ui.activeSelf);
+
+        if (ui.activeSelf)
+        {
+            Time.timeScale = 0.0F;
+        }
+        else
+        {
+            Time.timeScale = 1.0F;
+        }
+    }
+}
