5bdd518cd57d3315b5be7b089e81e191d26e88ca
Javy Wu <javywu1022@gmail.com>
2019-01-17 21:47:24 -0800

    test scene set up

---
+++ Groundbreakers/Assets/BuildManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    public GameObject standardCharacterPrefab;
+    private GameObject characterToPlace;
+
+    public static int characterCount = 0;
+
+    public static int maximumCharacter = 5;
+
+
+
+
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+    }
+
+    void Start()
+    {
+        this.characterToPlace = this.standardCharacterPrefab;
+
+    }
+
+
+    public GameObject GetCharacterToPlace()
+    {
+        return this.characterToPlace;
+    }
+
+}
---
+++ Groundbreakers/Assets/Nodes.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nodes : MonoBehaviour
+{
+    private SpriteRenderer rend;
+    public Color OnMouseColor;
+    private Color StartColor;
+    private GameObject character;
+    private bool empty = true;
+
+    void Start()
+    {
+        rend = gameObject.GetComponent<SpriteRenderer>();
+        this.StartColor = this.rend.color;
+
+
+    }
+
+    void OnMouseEnter()
+    {
+        this.rend.color = this.OnMouseColor;
+    }
+
+    void OnMouseDown()
+    {
+
+        if (this.character != null && this.empty == false)
+        {
+            Debug.Log("can't build there!");
+
+        }
+        else if (this.character == null && this.empty == true && BuildManager.characterCount < BuildManager.maximumCharacter)
+        {
+            this.empty = false;
+            GameObject characterToPlace = BuildManager.instance.GetCharacterToPlace();
+            character = (GameObject)Instantiate(characterToPlace, transform.position, transform.rotation);
+            BuildManager.characterCount += 1;
+        }
+
+        if (BuildManager.characterCount > BuildManager.maximumCharacter)
+        {
+            Debug.Log("Can't place more character!");
+        }
+
+
+    }
+
+
+
+    void OnMouseExit()
+    {
+        this.rend.color = this.StartColor;
+
+    }
+
+}
+
+
+
