fb04072f0b18184c94f2db9c0899ae086165c166
Chubblezap <ajwiley@ucsc.edu>
2019-01-19 21:56:03 -0800

    Firebat mostly done. Enemy_Generic implemented
    +Split the existing Firebat into Firebat_Script and Enemy_Generic
    +Added public damage and stun functions, and changed the projectile template to call these for testing.
    +Cleaned up some characterattack and rangedattack code to fit the new functionality
    +Switched the testing scene to spawn Firebats. Placeholder enemy is deprecated and should be removed soon.
    +Damage calculation considers the "Armored" attribute now
    +Animations added where possible: Movement, stun and death. Direction-switching code is commented out for later.

--- Groundbreakers/Assets/Scripts/Enemies/Fire_Bat_Script.cs
+++ Groundbreakers/Assets/Scripts/Enemies/Fire_Bat_Script.cs
@@ -5,132 +5,43 @@
 
     using UnityEngine;
 
+    [RequireComponent(typeof(Enemy_Generic))]
     public class Fire_Bat_Script : MonoBehaviour
     {
         #region Variable Declarations
 
         /// <summary>
-        /// Death effect object
-        /// </summary>
-        public GameObject deathEffect;
-
-        /// <summary>
-        /// Waypoint-storing object
-        /// </summary>
-        private Transform target;
-
-        /// <summary>
-        /// Animator declaration
-        /// </summary>
-        private Animator animator;
-
-        /// <summary>
         /// Enemy Stats
         /// </summary>
-        private float strength = 1;
-        private float health = 1;
+        private float health = 5;
         private float speed = 1f;
-        private int damage = 1;
-
-        /// <summary>
-        /// Status effects
-        /// </summary>
-        private bool isStunned = false;
-        private float stunTime = 0;
-
-        /// <summary>
-        /// Current waypoint based on waypoint array
-        /// </summary>
-        private int waypointIndex = -1;
+        private int power = 1;
+        private string[] attributes = new string[] { "Armored", "Flying" };
+        private Enemy_Generic enemy;
 
         #endregion
 
         void Start()
         {
-            this.GetNextWaypoint();
-            this.animator = this.GetComponent<Animator>();
+            enemy = this.GetComponent<Enemy_Generic>();
+            this.EnemyInit();
         }
 
         void Update()
         {
-            // Check health, die if health <= 0. Spawns a 'death effect'.
-            if (this.health <= 0)
+            // Passive effects go here.
+        }
+
+        // Initialization script
+        void EnemyInit()
+        {
+            this.enemy.health = this.health;
+            this.enemy.speed = this.speed;
+            this.enemy.power = this.power;
+            for (int i = 0; i < this.attributes.Length; i++)
             {
-                GameObject effect = (GameObject)Instantiate(this.deathEffect, this.transform.position, Quaternion.identity);
-                Destroy(effect, 1);
-                Destroy(this.gameObject);
+                this.enemy.attributes.Add(this.attributes[i]);
             }
-
-            if (!this.isStunned)
-            {
-                // Move the enemy towards the target waypoint
-                Vector2 dir = this.target.position - this.transform.position;
-                this.transform.Translate(dir.normalized * this.speed * Time.deltaTime, Space.World);
-
-                // Check if the waypoint has been reached
-                if (Vector2.Distance(this.transform.position, this.target.position) <= 0.01f)
-                {
-                    this.GetNextWaypoint();
-                }
-            }
-        }
-
-        // Stun handlers
-        void StunEnemy(float time)
-        {
-            this.isStunned = true;
-            this.animator.SetBool("Stun",true);
-            this.stunTime = time;
-            this.StartCoroutine(this.StunTick());
-        }
-
-        IEnumerator StunTick()
-        {
-            this.stunTime -= Time.deltaTime;
-            if (this.stunTime <= 0)
-            {
-                this.stunTime = 0;
-                this.isStunned = false;
-                this.animator.SetBool("Stun", false);
-                yield return 0;
-            } 
-        }
-
-        // Collision detector for projectiles
-        void OnCollisionEnter(Collision col)
-        {
-            // If projectile, destroy the projectile and do damage calculation. Firebat is ARMORED, and takes damage from armor penetration.
-            if (col.gameObject.tag == "Projectile")
-            {
-                Destroy(col.gameObject);
-                ////+Change this line to the proper projectile script+ health -= col.gameObject."ProjectileScript".armorpen
-            }
-        }
-
-        // Get the next waypoint and update the index
-        void GetNextWaypoint()
-        {
-            if (this.waypointIndex >= Waypoints.points.Length - 1)
-            {
-                this.EndPath();
-                return;
-            }
-            this.waypointIndex++;
-            this.target = Waypoints.points[this.waypointIndex];
-            /*
-            Vector2 dir = this.target.position - this.transform.position; // Change animation to fit the angle
-            if (Mathf.Abs(dir.y) > Mathf.Abs(dir.x) && dir.y > 0) { this.animator.SetInteger("Direction", 0); } // Up
-            else if (Mathf.Abs(dir.y) < Mathf.Abs(dir.x) && dir.x > 0) { this.animator.SetInteger("Direction", 1); } // Right
-            else if (Mathf.Abs(dir.y) > Mathf.Abs(dir.x) && dir.y < 0) { this.animator.SetInteger("Direction", 2); } // Down
-            else if (Mathf.Abs(dir.y) < Mathf.Abs(dir.x) && dir.x < 0) { this.animator.SetInteger("Direction", 3); } // Left
-            */
-        }
-
-        // When the enemy reaches the end of its path
-        void EndPath()
-        {
-            HP.healthPoint -= this.damage;
-            Destroy(this.gameObject);
         }
     }
 }
--- Groundbreakers/Assets/Scripts/rangeattack.cs
+++ Groundbreakers/Assets/Scripts/rangeattack.cs
@@ -1,23 +1,20 @@
-﻿using UnityEngine;
+﻿using Assets.Enemies.Scripts;
+
+using UnityEngine;
 
 public class rangeattack : MonoBehaviour
 {
     public float speed = 10f;
 
-    private Transform target;
+    public Transform target;
     
     public int damage = 1;
-    private Enemy_movement enemy;
-    Enemy_movement bullet;
+
+    public int armorpen = 2;
 
 
     public void chase(Transform _target) {
         this.target = _target;
-    }
-
-   
-    void hitTarget() {
-        Destroy(this.gameObject);
     }
 
     void Update() {
@@ -30,30 +27,17 @@ public class rangeattack : MonoBehaviour
         Vector3 direction = this.target.position - this.transform.position;
         float distancePerFrame = this.speed * Time.deltaTime;
 
-        if (direction.magnitude <= distancePerFrame)
-        {
-            this.hitTarget();
-
-        }
-
         this.transform.Translate(direction.normalized * distancePerFrame, Space.World);
     }
 
-    // Deals damage to the enemies, but sometimes this.enemy.health is not updated ?
-    void OnTriggerEnter2D(Collider2D target)
+    // Deals damage to the enemies
+    void OnTriggerEnter2D(Collider2D hitTarget)
     {
-        
-        if (target.gameObject.tag == "Enemy")
+        if (hitTarget.gameObject.tag == "Enemy")
         {
-            enemy = GameObject.FindObjectOfType<Enemy_movement>();
-            if (enemy != null && this.enemy.health >= 0)
-            {
-                
-                this.enemy.health -= damage;
-                Debug.Log(enemy.health);
-            }
-            
-
+            hitTarget.gameObject.GetComponent<Enemy_Generic>().DamageEnemy(this.damage, this.armorpen);
+            hitTarget.gameObject.GetComponent<Enemy_Generic>().StunEnemy((float)0.2);
+            Destroy(this.gameObject);
         }
     }
 
