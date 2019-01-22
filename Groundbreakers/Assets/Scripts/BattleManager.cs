b39b8f5e57e1ca1035f788f10a5bf89f95d1c967
RaiderSoap <info@chivalrystudio.com>
2019-01-21 22:43:11 -0800

    Implement sample tilemap enter animation

---
+++ Groundbreakers/Assets/Scripts/BattleManager.cs
@@ -0,0 +1,71 @@
+﻿namespace Assets.Scripts
+{
+    using UnityEngine;
+
+    public class BattleManager : MonoBehaviour
+    {
+        #region Inspector Variables
+
+        [SerializeField]
+        private GameObject tilePrefab;
+
+        #endregion
+
+        #region Internal Variables
+
+        private const uint Dimension = 8;
+
+        private const float CellSize = 0.5f;
+
+        /// <summary>
+        /// The parent transform that hold all sub sprite for tiles.
+        /// </summary>
+        private Transform boardHolder;
+
+        #endregion
+
+        #region Unity Callbacks
+
+        public void Start()
+        {
+            this.CreateGameBoard();
+        }
+
+        public void Update()
+        {
+        }
+
+        #endregion
+
+        #region Internal functions
+
+        private void CreateGameBoard()
+        {
+            this.boardHolder = new GameObject("Map").transform;
+
+            // Spawning each individual tiles
+            for (var i = 0; i < Dimension; i++)
+            {
+                for (var j = 0; j < Dimension; j++)
+                {
+                    var instance = Instantiate(
+                        this.tilePrefab, 
+                        new Vector3(i * CellSize, j * CellSize, 0f), 
+                        Quaternion.identity);
+
+                    instance.transform.SetParent(this.boardHolder);
+                }
+            }
+
+            // Locating the mother board
+            float newX = -Dimension * CellSize / 2f; 
+            float newY = -Dimension * CellSize / 2f;
+
+            this.boardHolder.SetPositionAndRotation(
+                new Vector3(newX, newY, 0f),
+                Quaternion.identity);
+        }
+
+        #endregion
+    }
+}
---
+++ Groundbreakers/Assets/Scripts/TileBlock.cs
@@ -0,0 +1,67 @@
+﻿namespace Assets.Scripts
+{
+    using System.Collections;
+    using UnityEngine;
+
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class TileBlock : MonoBehaviour
+    {
+        #region Internal Variables
+
+        private const float TempOffset = 7.0f;
+
+        private Rigidbody2D rb2D;
+
+        private Vector3 originalPosition;
+
+        #endregion
+
+        #region Unity Callbacks
+
+        private void OnEnable()
+        {
+            this.rb2D = this.GetComponent<Rigidbody2D>();
+            this.rb2D.gravityScale = 0f;
+        }
+
+        private void Start()
+        {
+            // Saving starting position
+            this.originalPosition = this.transform.position;
+
+            this.transform.SetPositionAndRotation(
+                new Vector3(this.originalPosition.x, this.originalPosition.y - TempOffset),
+                Quaternion.identity);
+
+            this.StartCoroutine(this.StartDropping());
+        }
+
+        private void FixedUpdate()
+        {
+            var delta = Mathf.Abs(this.gameObject.transform.position.y - this.originalPosition.y);
+
+            // TODO: add bouncing effect
+            if (delta < 0.1f)
+            {
+                this.rb2D.gravityScale = 0f;
+                this.rb2D.velocity = Vector3.zero;
+                this.transform.SetPositionAndRotation(this.originalPosition, Quaternion.identity);
+            }
+        }
+
+        #endregion
+
+        #region Internal Functions
+
+        private IEnumerator StartDropping()
+        {
+            var time = Random.Range(0.0f, 5.0f);
+
+            yield return new WaitForSeconds(time);
+
+            this.rb2D.gravityScale = -0.3f;
+        }
+
+        #endregion
+    }
+}
