be6c35c86b8d1d79cea75c648b638ee23720a16a
Chris Kong <cxkong@ucsc.edu>
2019-01-21 19:50:11 -0800

    Moved files around

---
+++ Groundbreakers/Assets/Scripts/Characters/TricksterBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TricksterBehavior : MonoBehaviour
+{
+    //character attributes
+    public int POW = 1;
+    public int ROF = 1;
+    public int RNG = 2;
+    public int MOB = 3;
+    public int AMP = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
