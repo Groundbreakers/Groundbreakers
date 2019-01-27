b1e93a95b2bd1cc71ec1ad69fc6b3bacd6eccebe
javyw <javywu1022@gmail.com>
2019-01-27 00:01:12 -0800

    optimalize deploy remove function and bug fixed

--- Groundbreakers/Assets/Scripts/DeployCharacter.cs
+++ Groundbreakers/Assets/Scripts/DeployCharacter.cs
@@ -14,7 +14,9 @@ public class DeployCharacter : MonoBehaviour
 
     public int maximumCharacter;
 
-    private GameObject character = null; // by default each node has no character on it
+    private GameObject character;
+
+    private GameObject character2;
 
     private GameObject parent;
 
@@ -25,48 +27,63 @@ public class DeployCharacter : MonoBehaviour
     private Color startColor;
 
     //private string type;
-
+    
 
     // remove character with right mouse click on it
     void removeCharacter()
     {
-        if (Input.GetMouseButtonDown(1) && this.character != null)
+        if (Input.GetMouseButtonDown(1))
         {
-           Destroy(this.character);
+            activeSetFalseAll();
         }
     }
 
     // deploy character with key 1~5 on the node the cursor is on
     void spawnCharacter()
     {
-        if (this.parent.transform.childCount < this.maximumCharacter)
-        {
-            if (Input.GetKeyDown("1") && this.character == null )
+        
+            if (Input.GetKeyDown("1") && isEmpty() == true)
             {
-                this.character = (GameObject)Instantiate(this.Character1, this.transform.position, this.transform.rotation);
-                this.character.transform.parent = this.parent.transform;
+                this.character.SetActive(true);
+               
             }
-            else if (Input.GetKeyDown("2") && this.character == null)
+            else if (Input.GetKeyDown("2") && isEmpty() == true)
             {
-                this.character = (GameObject)Instantiate(this.Character2, this.transform.position, this.transform.rotation);
-                this.character.transform.parent = this.parent.transform;
+                this.character2.SetActive(true);
+
             }
-            else if (Input.GetKeyDown("3") && this.character == null)
-            {
-                this.character = (GameObject)Instantiate(this.Character3, this.transform.position, this.transform.rotation);
-                this.character.transform.parent = this.parent.transform;
-            }
-            else if (Input.GetKeyDown("4") && this.character == null)
-            {
-                this.character = (GameObject)Instantiate(this.Character4, this.transform.position, this.transform.rotation);
-                this.character.transform.parent = this.parent.transform;
-            }
-            else if (Input.GetKeyDown("5") && this.character == null)
-            {
-                this.character = (GameObject)Instantiate(this.Character5, this.transform.position, this.transform.rotation);
-                this.character.transform.parent = this.parent.transform;
-            }
-        }
+        /* else if (Input.GetKeyDown("2") && this.character == null)
+         {
+             this.character = (GameObject)Instantiate(this.Character2, this.transform.position, this.transform.rotation);
+             this.character.transform.parent = this.parent.transform;
+         }
+         else if (Input.GetKeyDown("3") && this.character == null)
+         {
+             this.character = (GameObject)Instantiate(this.Character3, this.transform.position, this.transform.rotation);
+             this.character.transform.parent = this.parent.transform;
+         }
+         else if (Input.GetKeyDown("4") && this.character == null)
+         {
+             this.character = (GameObject)Instantiate(this.Character4, this.transform.position, this.transform.rotation);
+             this.character.transform.parent = this.parent.transform;
+         }
+         else if (Input.GetKeyDown("5") && this.character == null)
+         {
+             this.character = (GameObject)Instantiate(this.Character5, this.transform.position, this.transform.rotation);
+             this.character.transform.parent = this.parent.transform;
+         }*/
+
+    }
+
+    private bool isEmpty() {
+
+        if (this.character.activeSelf == false && this.character2.activeSelf == false) return true;
+        else return false;
+    }
+
+    private void activeSetFalseAll() {
+        this.character.SetActive(false);
+        this.character2.SetActive(false);
     }
 
     void Start()
@@ -74,6 +91,17 @@ public class DeployCharacter : MonoBehaviour
         this.parent = GameObject.Find("character");
         rend = GetComponent<SpriteRenderer>();
         startColor = rend.color;
+        
+        this.character = (GameObject)Instantiate(this.Character1, this.transform.position, this.transform.rotation);
+        this.character.SetActive(false);
+
+        this.character2 = (GameObject)Instantiate(this.Character2, this.transform.position, this.transform.rotation);
+        this.character2.SetActive(false);
+
+
+
+
+
     }
 
     void Update()
