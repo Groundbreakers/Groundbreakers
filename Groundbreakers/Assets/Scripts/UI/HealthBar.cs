bfd9731ae60e4cc6a4368d0a208715300f42d720
Yuming <lionpkqq@gmail.com>
2019-01-28 14:56:16 -0800

    Playable Update 3

---
+++ Groundbreakers/Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Enemies.Scripts;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image healthBar;
+    public Enemy_Generic enemy;
+
+    void Start()
+    {
+        this.healthBar = this.GetComponent<Image>();
+        this.enemy = this.transform.parent.parent.parent.gameObject.GetComponent<Enemy_Generic>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.healthBar.fillAmount = (float)this.enemy.health / (float)this.enemy.maxHealth;
+    }
+}
