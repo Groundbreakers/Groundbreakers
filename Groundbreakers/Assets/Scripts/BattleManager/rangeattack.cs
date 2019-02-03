7b49e5d7a205733e5fe5cbe3e05331d44140f815
Javy Wu <javywu1022@gmail.com>
2019-02-02 23:46:11 -0800

    bullets now don't track enemies

--- Groundbreakers/Assets/Scripts/BattleManager/rangeattack.cs
+++ Groundbreakers/Assets/Scripts/BattleManager/rangeattack.cs
@@ -1,23 +1,43 @@
 ﻿using System;
-
 using Assets.Enemies.Scripts;
-
 using UnityEngine;
 
 public class rangeattack : MonoBehaviour
 {
+    public int armorpen = 2;
+
+    public int damage = 10;
+
+    public bool hit;
+
     public float speed = 70f;
 
     public Transform target;
-    
-    public int damage = 10;
 
-    public int armorpen = 2;
-
-    public Boolean hit;
+    private Vector2 direction;
 
     public void chase(Transform _target) {
         this.target = _target;
+    }
+
+    // Deals damage to the enemies
+    void OnTriggerEnter2D(Collider2D hitTarget) {
+        if (hitTarget.gameObject.tag == "Enemy")
+        {
+            if (!this.hit)
+            {
+                hitTarget.gameObject.GetComponent<Enemy_Generic>().DamageEnemy(this.damage, this.armorpen, 1, false);
+
+                // hitTarget.gameObject.GetComponent<Enemy_Generic>().StunEnemy((float)0.2);
+            }
+
+            this.hit = true;
+            Destroy(this.gameObject);
+        }
+    }
+
+    void Start() {
+        this.direction = this.target.position - this.transform.position;
     }
 
     void Update() {
@@ -27,24 +47,9 @@ public class rangeattack : MonoBehaviour
             return;
         }
 
-        Vector3 direction = this.target.position - this.transform.position;
         float distancePerFrame = this.speed * Time.deltaTime;
 
-        transform.Translate(direction.normalized * distancePerFrame, Space.World);
-    }
-      
-    // Deals damage to the enemies
-    void OnTriggerEnter2D(Collider2D hitTarget)
-    {
-        if (hitTarget.gameObject.tag == "Enemy")
-        {
-            if (!hit)
-            {
-                hitTarget.gameObject.GetComponent<Enemy_Generic>().DamageEnemy(this.damage, this.armorpen, 1, false);
-                //hitTarget.gameObject.GetComponent<Enemy_Generic>().StunEnemy((float)0.2);
-            }
-            this.hit = true;
-            Destroy(this.gameObject);
-        }
+        // transform.Translate(direction.normalized * distancePerFrame, Space.World); // if bullet tracking
+        this.transform.Translate(Time.deltaTime * this.direction.normalized * distancePerFrame, Space.World);
     }
 }
