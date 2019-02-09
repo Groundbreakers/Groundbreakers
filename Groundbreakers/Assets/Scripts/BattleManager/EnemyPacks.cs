cbb68ff92e781dd56be45ce03fc298f97cdd45e5
RaiderSoap <info@chivalrystudio.com>
2019-02-08 22:22:16 -0800

    Attempted to refactor some of the code

---
+++ Groundbreakers/Assets/Scripts/BattleManager/EnemyPacks.cs
@@ -0,0 +1,40 @@
+﻿namespace Asset.Script
+{
+    using System;
+    using System.Collections.Generic;
+
+    using UnityEngine;
+
+    public class EnemyPacks : MonoBehaviour
+    {
+        #region Inspector Fields
+
+        [SerializeField]
+        private Transform enemyPrefab;
+
+        #endregion
+
+        #region Internal Fields
+
+        /// <summary>
+        /// The packs, SUBJECT TO CHANGE
+        /// </summary>
+        [Obsolete]
+        private List<Transform>[] packs = new List<Transform>[2];
+
+        #endregion 
+
+        private void Awake()
+        {
+            // Well, eventually we ready from an XML/JSON File
+            this.InitializePacks();
+        }
+
+        private void InitializePacks()
+        {
+            this.packs[0] = new List<Transform>(new Transform[] { this.enemyPrefab, this.enemyPrefab, this.enemyPrefab, this.enemyPrefab, this.enemyPrefab });
+            this.packs[1] = new List<Transform>(new Transform[] { this.enemyPrefab, this.enemyPrefab, this.enemyPrefab });
+        }
+    }
+
+}
--- Groundbreakers/Assets/Scripts/BattleManager/WaveSpawner.cs
+++ Groundbreakers/Assets/Scripts/BattleManager/WaveSpawner.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 using Assets.Enemies.Scripts;
 
@@ -8,44 +9,35 @@ using UnityEngine;
 
 using Random = UnityEngine.Random;
 
+[RequireComponent(typeof(PackLists))]
 public class WaveSpawner : MonoBehaviour
 {
-    public Vector3 spawnPoint1;
-    public Vector3 spawnPoint2;
-
-    // Waypoint lists
-    public List<Vector3> path1 = new List<Vector3>();
-    public List<Vector3> path2 = new List<Vector3>();
+    #region Internel Fields
 
     // Packs for paths 1 and 2. Stored as an array of Lists of prefabs.
-    public List<Transform>[] pack1 = new List<Transform>[5];
-    public List<Transform>[] pack2 = new List<Transform>[5];
+    private List<Transform>[] pack1 = new List<Transform>[5];
 
-    void Start()
+    private List<Transform>[] pack2 = new List<Transform>[5];
+
+    // Waypoint lists
+    private List<Vector3> path1 = new List<Vector3>();
+
+    private List<Vector3> path2 = new List<Vector3>();
+
+    public Vector3 spawnPoint1;
+
+    public Vector3 spawnPoint2;
+
+    #endregion
+
+    #region Unity Callbacks
+
+    private void Start()
     {
-        GetNewPacks(0,1);
+        this.GetNewPacks(0, 1);
     }
 
-    // Spawns a wave at each lane, then increments the wave number.
-    public IEnumerator SpawnWave(int num)
-    {
-        Transform thisEnemy;
-        for (int i = 0; i < this.pack1[num].Count || i < this.pack2[num].Count; i++)
-        {
-            if (i < this.pack1[num].Count)
-            {
-                thisEnemy = Instantiate(this.pack1[num][i], this.spawnPoint1, Quaternion.identity);
-                thisEnemy.GetComponent<Enemy_Generic>().waypointList = this.path1;
-            }
-
-            if (i < this.pack2[num].Count)
-            {
-                thisEnemy = Instantiate(this.pack2[num][i], this.spawnPoint2, Quaternion.identity);
-                thisEnemy.GetComponent<Enemy_Generic>().waypointList = this.path2;
-            }
-            yield return new WaitForSeconds(1f);
-        }
-    }
+    #endregion
 
     // Adds a waypoint to a path. Presumably called by the terrain generator.
     public void AddPoint(Vector3 point, int path)
@@ -60,11 +52,10 @@ public class WaveSpawner : MonoBehaviour
         }
     }
 
-    // Gets the packs for a level. Use lowerRange and upperRange to specify which packs to use.
     public void GetNewPacks(int lowerRange, int upperRange)
     {
         int diceroll1, diceroll2;
-        for (int i = 0; i < 5; i++)
+        for (var i = 0; i < 5; i++)
         {
             diceroll1 = Random.Range(lowerRange, upperRange + 1);
             this.pack1[i] = new List<Transform>();
@@ -74,5 +65,50 @@ public class WaveSpawner : MonoBehaviour
             this.pack2[i] = new List<Transform>();
             this.pack2[i].AddRange(this.GetComponent<PackLists>().Packs[diceroll2]);
         }
+    }
+
+    /// <summary>
+    /// The main function that spawns the 5 waves
+    /// </summary>
+    /// <param name="waveNumber">
+    /// The num.
+    /// </param>
+    /// <returns>
+    /// The <see cref="IEnumerator"/>.
+    /// </returns>
+    public IEnumerator SpawnWave(int waveNumber)
+    {
+        Transform thisEnemy;
+        for (var i = 0; i < this.pack1[waveNumber].Count || i < this.pack2[waveNumber].Count; i++)
+        {
+            if (i < this.pack1[waveNumber].Count)
+            {
+                thisEnemy = Instantiate(this.pack1[waveNumber][i], this.spawnPoint1, Quaternion.identity);
+                thisEnemy.GetComponent<Enemy_Generic>().waypointList = this.path1;
+
+            }
+
+            if (i < this.pack2[waveNumber].Count)
+            {
+                thisEnemy = Instantiate(this.pack2[waveNumber][i], this.spawnPoint2, Quaternion.identity);
+                thisEnemy.GetComponent<Enemy_Generic>().waypointList = this.path2;
+            }
+
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    private void InstantiateEnemyAtSpawnPoint(Transform minion, int pathId)
+    {
+        var startingPoint = this.transform.position;
+
+        var instance = Instantiate(
+            minion,
+            startingPoint,
+            Quaternion.identity);
+
+        // Set enemies path
+        instance.GetComponent<Enemy_Generic>().waypointList = pathId == 0 ? this.path1 : this.path2;
+        instance.transform.SetParent(this.transform.parent);
     }
 }
