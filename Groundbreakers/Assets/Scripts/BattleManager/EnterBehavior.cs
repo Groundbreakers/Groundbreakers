41be26e8d58144c9d2377b3f56d231ab2c4d7070
RaiderSoap <info@chivalrystudio.com>
2019-02-09 20:46:26 -0800

    [Feature] Better Tile Entering animation

--- Groundbreakers/Assets/Scripts/BattleManager/EnterBehavior.cs
+++ Groundbreakers/Assets/Scripts/BattleManager/EnterBehavior.cs
@@ -1,50 +1,56 @@
 ﻿namespace Assets.Scripts
 {
+    using DG.Tweening;
+
     using UnityEngine;
 
     /// <summary>
-    /// This component handles tile enter/exiting animation. Attaching this component to objects
-    /// that you wish to falling and fade.
+    ///     This component handles tile enter/exiting animation. Attaching this component to object
+    ///     that you wish to falling and fade.
     /// </summary>
-    [RequireComponent(typeof(Rigidbody2D))]
     public class EnterBehavior : MonoBehaviour
     {
-        #region Inspector Fields
+        #region Inspector Values
 
         [SerializeField]
-        private Vector2 offset;
+        private Vector3 offset;
+
+        [SerializeField]
+        [Range(0.5f, 5.0f)]
+        private float enterDuration;
 
         #endregion
 
-        #region Internal fields
+        #region Internal Fields
 
+        private SpriteRenderer sprite;
 
-
-        #endregion
-
-        #region Public Funcitons
-
-
+        private Vector3 originalPosition;
 
         #endregion
 
         #region Unity Callbacks
 
+        private void OnEnable()
+        {
+            // Init transform
+            this.originalPosition = this.transform.position;
+            this.transform.Translate(this.offset);
+
+            // Init sprite 
+            this.sprite = this.GetComponent<SpriteRenderer>();
+            this.sprite.color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+        }
+
         private void Start()
         {
+            // Let DOTween handle the animation.
+            this.transform.DOMove(this.originalPosition, this.enterDuration)
+                    .SetEase(Ease.OutBack);
 
+            this.sprite.DOFade(1.0f, this.enterDuration)
+                    .SetEase(Ease.OutExpo);
         }
-
-        private void FixedUpdate()
-        {
-            
-        }
-
-        #endregion
-
-        #region Internal Functions
-
-
 
         #endregion
     }
