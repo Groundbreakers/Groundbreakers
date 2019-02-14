716cb7d0c52137416a372170c84b30249379cbf4
RaiderSoap <info@chivalrystudio.com>
2019-02-14 12:44:50 -0800

    Introducing Path atlas

---
+++ Groundbreakers/Assets/Scripts/BattleManager/PathAtlas.cs
@@ -0,0 +1,32 @@
+﻿namespace Assets.Scripts
+{
+    using UnityEngine;
+    using UnityEngine.U2D;
+
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class PathAtlas : MonoBehaviour
+    {
+        #region Inspector Values
+
+        [SerializeField]
+        private SpriteAtlas spriteAtlas;
+
+        #endregion
+
+        #region Internal Variables
+
+        private SpriteRenderer sprite;
+
+        #endregion
+
+        #region Unity Callbacks
+
+        public void OnEnable()
+        {
+            this.sprite = this.GetComponent<SpriteRenderer>();
+            this.sprite.sprite = this.spriteAtlas.GetSprite("road_1");
+        }
+
+        #endregion
+    }
+}
