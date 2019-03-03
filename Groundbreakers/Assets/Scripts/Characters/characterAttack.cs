e312be745d985afc8c907d978d275b9fc3411cc5
javyw <javywu1022@gmail.com>
2019-03-03 02:43:08 -0800

    reformated

--- Groundbreakers/Assets/Scripts/Characters/characterAttack.cs
+++ Groundbreakers/Assets/Scripts/Characters/characterAttack.cs
@@ -1,11 +1,8 @@
-﻿using System.Collections.Generic;
-
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class characterAttack : MonoBehaviour
 {
-    public Animator animator;
-
     public string enemyTag = "Enemy";
 
     public float fireRate = 1f;
@@ -16,24 +13,208 @@ public class characterAttack : MonoBehaviour
 
     public GameObject rangeAttackPrefab;
 
+    public Animator animator;
+
     public string stance = "Gun";
 
-    private string attackMode = "default";
-
-    private float fireCountdown = 0f;
-
-    private Vector3 firePoint;
-
+    private characterAttributes trickster;
     private bool isChanging = false;
-
-    // draw the attack range of the character selected
-    private CircleCollider2D myCollider;
+    private Vector3 firePoint;
+    private float fireCountdown = 0f;
 
     private Transform target;
 
-    private List<GameObject> targetedEnemies;
+    private string attackMode = "default";
 
-    private characterAttributes trickster;
+    private List<GameObject> targetedEnemies;
+    // draw the attack range of the character selected
+
+    private CircleCollider2D myCollider;
+
+    void Awake()
+    {
+        targetedEnemies = new List<GameObject>();
+        myCollider = GetComponent<CircleCollider2D>();
+        trickster = GetComponent<characterAttributes>();
+        firePoint = rangeAttackFirepoint.position;
+
+    }
+
+    void Start()
+    {
+        fireRate = trickster.ROF * .5f;
+        animator.SetFloat("FireRate", fireRate);
+        myCollider.radius = trickster.RNG + .5f;
+    }
+
+    void Update()
+    {
+        if (target != null && !isChanging)
+        {
+            //calculate angle
+            Vector2 direction = target.transform.position - this.transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            if (angle < 0)
+            {
+                angle = angle + 360f;
+            }
+
+            //check if it's pointing right
+            if (!trickster.disabled)
+            {
+                if ((angle <= 360 && angle >= 315) || (angle >= 0 && angle < 45))
+                {
+                    animator.SetBool("FacingRight", true);
+                    animator.SetBool("FacingLeft", false);
+                    animator.SetBool("FacingUp", false);
+                    animator.SetBool("FacingDown", false);
+                    firePoint = new Vector3(gameObject.transform.position.x + .475f, gameObject.transform.position.y + .5f, gameObject.transform.position.z);
+                }
+                else if (angle >= 45 && angle < 135) //check if it's pointing up
+                {
+                    animator.SetBool("FacingRight", false);
+                    animator.SetBool("FacingLeft", false);
+                    animator.SetBool("FacingUp", true);
+                    animator.SetBool("FacingDown", false);
+                    firePoint = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + .6f, gameObject.transform.position.z);
+                }
+                else if (angle >= 135 && angle < 225) //check if it's pointing left
+                {
+                    animator.SetBool("FacingRight", false);
+                    animator.SetBool("FacingLeft", true);
+                    animator.SetBool("FacingUp", false);
+                    animator.SetBool("FacingDown", false);
+                    firePoint = new Vector3(gameObject.transform.position.x - .475f, gameObject.transform.position.y + .5f, gameObject.transform.position.z);
+                }
+                else if (angle >= 225 && angle < 315) //check if it's pointing down
+                {
+                    animator.SetBool("FacingRight", false);
+                    animator.SetBool("FacingLeft", false);
+                    animator.SetBool("FacingUp", false);
+                    animator.SetBool("FacingDown", true);
+                    firePoint = new Vector3(gameObject.transform.position.x - .05f, gameObject.transform.position.y - .05f, gameObject.transform.position.z);
+                }
+                //Debug.Log(angle);
+            }
+
+        }
+
+        if (!isChanging && !trickster.disabled)
+        {
+            this.fireCount();
+        }
+
+        if (trickster.disabled)
+        {
+            myCollider.radius = 0;
+            target = null;
+        }
+
+        if (!animator.GetBool("Transition"))
+        {
+            isChanging = false;
+
+        }
+    }
+
+    //if an enemy enters in range
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy")
+        {
+            targetedEnemies.Add(other.gameObject);
+            updateTarget();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Enemy")
+        {
+            targetedEnemies.Remove(other.gameObject);
+            updateTarget();
+        }
+    }
+
+    // update the closest target in range
+    void updateTarget()
+    {
+        if (this.attackMode == "default") defaultMode();
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetKeyDown("r"))
+        {
+            this.attackMode = "multi-shot";
+            //Debug.Log(this.attackMode);
+        }
+        else if (Input.GetKeyDown("n"))
+        {
+            this.attackMode = "default";
+            //Debug.Log(this.attackMode);
+        }
+    }
+
+    void fireCount()
+    {
+        myCollider.radius = trickster.RNG + .5f; // or whatever radius you want.
+        if (this.target == null)
+        {
+            animator.SetBool("Firing", false);
+            return;
+        }
+
+        if (this.fireCountdown <= 0f)
+        {
+            animator.SetBool("Firing", true);
+            this.shoot();
+
+            this.fireCountdown = 1f / this.fireRate;
+        }
+
+        this.fireCountdown -= Time.deltaTime;
+    }
+
+    // Instantiate and and chase the target
+    void shoot()
+    {
+        GameObject rangeAttack_object = (GameObject)Instantiate(
+            this.rangeAttackPrefab,
+            firePoint,
+            this.rangeAttackFirepoint.rotation);
+
+        rangeattack rangeattack = rangeAttack_object.GetComponent<rangeattack>();
+        if (stance.Equals("Melee"))
+        {
+            rangeattack.speed = 10f;
+            rangeAttack_object.GetComponent<SpriteRenderer>().enabled = false;
+        }
+        else
+        {
+            rangeattack.speed = 3f;
+        }
+        if (rangeattack != null)
+        {
+            this.setProjectileStatusAttributes(rangeattack);
+            rangeattack.updateStats(trickster.POW, trickster.AMP);
+            rangeattack.chase(this.target);
+        }
+    }
+
+    void defaultMode()
+    {
+        if (targetedEnemies.Count != 0)
+        {
+            target = targetedEnemies[0].transform;
+        }
+        else
+        {
+            target = null;
+        }
+
+    }
 
     public void change()
     {
@@ -43,99 +224,24 @@ public class characterAttack : MonoBehaviour
             GameObject.Find("CharactersPanel").GetComponent<CharacterManager>().UpdatePanel();
         }
 
-        this.isChanging = true;
-        if (this.stance.Equals("Melee"))
+        isChanging = true;
+        if (stance.Equals("Melee"))
         {
-            this.stance = "Gun";
-            this.animator.SetBool("Transition", true);
-            this.animator.SetBool("Sitting", true);
-            this.animator.SetBool("Standing", false);
-            this.trickster.gun();
-            this.myCollider.radius = this.trickster.RNG + .5f; // or whatever radius you want.
+            stance = "Gun";
+            animator.SetBool("Transition", true);
+            animator.SetBool("Sitting", true);
+            animator.SetBool("Standing", false);
+            trickster.gun();
+            myCollider.radius = trickster.RNG + .5f; // or whatever radius you want.
         }
         else
         {
-            this.stance = "Melee";
-            this.animator.SetBool("Transition", true);
-            this.animator.SetBool("Sitting", false);
-            this.animator.SetBool("Standing", true);
-            this.trickster.melee();
-            this.myCollider.radius = this.trickster.RNG + .5f; // or whatever radius you want.
-        }
-    }
-
-    void Awake()
-    {
-        this.targetedEnemies = new List<GameObject>();
-        this.myCollider = this.GetComponent<CircleCollider2D>();
-        this.trickster = this.GetComponent<characterAttributes>();
-        this.firePoint = this.rangeAttackFirepoint.position;
-    }
-
-    void defaultMode()
-    {
-        if (this.targetedEnemies.Count != 0)
-        {
-            this.target = this.targetedEnemies[0].transform;
-        }
-        else
-        {
-            this.target = null;
-        }
-    }
-
-    void fireCount()
-    {
-        this.myCollider.radius = this.trickster.RNG + .5f; // or whatever radius you want.
-        if (this.target == null)
-        {
-            this.animator.SetBool("Firing", false);
-            return;
-        }
-
-        if (this.fireCountdown <= 0f)
-        {
-            this.animator.SetBool("Firing", true);
-            this.shoot();
-
-            this.fireCountdown = 1f / this.fireRate;
-        }
-
-        this.fireCountdown -= Time.deltaTime;
-    }
-
-    void OnMouseOver()
-    {
-        if (Input.GetKeyDown("r"))
-        {
-            this.attackMode = "multi-shot";
-
-            // Debug.Log(this.attackMode);
-        }
-        else if (Input.GetKeyDown("n"))
-        {
-            this.attackMode = "default";
-
-            // Debug.Log(this.attackMode);
-        }
-    }
-
-    // if an enemy enters in range
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.tag == "Enemy")
-        {
-            this.targetedEnemies.Add(other.gameObject);
-            this.updateTarget();
-        }
-    }
-
-    void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.tag == "Enemy")
-        {
-            this.targetedEnemies.Remove(other.gameObject);
-            this.updateTarget();
+            stance = "Melee";
+            animator.SetBool("Transition", true);
+            animator.SetBool("Sitting", false);
+            animator.SetBool("Standing", true);
+            trickster.melee();
+            myCollider.radius = trickster.RNG + .5f; // or whatever radius you want.
         }
     }
 
@@ -149,131 +255,5 @@ public class characterAttack : MonoBehaviour
         if (this.trickster.breakSE == true) rangeattack.setBreak();
         if (this.trickster.netSE == true) rangeattack.setNet();
         if (this.trickster.purgeSE == true) rangeattack.setPurge();
-    }
-
-    // Instantiate and and chase the target
-    void shoot()
-    {
-        GameObject rangeAttack_object = (GameObject)Instantiate(
-            this.rangeAttackPrefab,
-            this.firePoint,
-            this.rangeAttackFirepoint.rotation);
-
-        rangeattack rangeattack = rangeAttack_object.GetComponent<rangeattack>();
-        if (this.stance.Equals("Melee"))
-        {
-            rangeattack.speed = 10f;
-            rangeAttack_object.GetComponent<SpriteRenderer>().enabled = false;
-        }
-        else
-        {
-            rangeattack.speed = 3f;
-        }
-
-        if (rangeattack != null)
-        {
-            this.setProjectileStatusAttributes(rangeattack);
-            rangeattack.updateStats(this.trickster.POW, this.trickster.AMP);
-
-            rangeattack.chase(this.target);
-        }
-    }
-
-    void Start()
-    {
-        this.fireRate = this.trickster.ROF * .5f;
-        this.animator.SetFloat("FireRate", this.fireRate);
-        this.myCollider.radius = this.trickster.RNG + .5f;
-    }
-
-    void Update()
-    {
-        if (this.target != null && !this.isChanging)
-        {
-            // calculate angle
-            Vector2 direction = this.target.transform.position - this.transform.position;
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
-            if (angle < 0)
-            {
-                angle = angle + 360f;
-            }
-
-            // check if it's pointing right
-            if (!this.trickster.disabled)
-            {
-                if ((angle <= 360 && angle >= 315) || (angle >= 0 && angle < 45))
-                {
-                    this.animator.SetBool("FacingRight", true);
-                    this.animator.SetBool("FacingLeft", false);
-                    this.animator.SetBool("FacingUp", false);
-                    this.animator.SetBool("FacingDown", false);
-                    this.firePoint = new Vector3(
-                        this.gameObject.transform.position.x + .475f,
-                        this.gameObject.transform.position.y + .5f,
-                        this.gameObject.transform.position.z);
-                }
-                else if (angle >= 45 && angle < 135)
-                {
-                    // check if it's pointing up
-                    this.animator.SetBool("FacingRight", false);
-                    this.animator.SetBool("FacingLeft", false);
-                    this.animator.SetBool("FacingUp", true);
-                    this.animator.SetBool("FacingDown", false);
-                    this.firePoint = new Vector3(
-                        this.gameObject.transform.position.x,
-                        this.gameObject.transform.position.y + .6f,
-                        this.gameObject.transform.position.z);
-                }
-                else if (angle >= 135 && angle < 225)
-                {
-                    // check if it's pointing left
-                    this.animator.SetBool("FacingRight", false);
-                    this.animator.SetBool("FacingLeft", true);
-                    this.animator.SetBool("FacingUp", false);
-                    this.animator.SetBool("FacingDown", false);
-                    this.firePoint = new Vector3(
-                        this.gameObject.transform.position.x - .475f,
-                        this.gameObject.transform.position.y + .5f,
-                        this.gameObject.transform.position.z);
-                }
-                else if (angle >= 225 && angle < 315)
-                {
-                    // check if it's pointing down
-                    this.animator.SetBool("FacingRight", false);
-                    this.animator.SetBool("FacingLeft", false);
-                    this.animator.SetBool("FacingUp", false);
-                    this.animator.SetBool("FacingDown", true);
-                    this.firePoint = new Vector3(
-                        this.gameObject.transform.position.x - .05f,
-                        this.gameObject.transform.position.y - .05f,
-                        this.gameObject.transform.position.z);
-                }
-
-                // Debug.Log(angle);
-            }
-        }
-
-        if (!this.isChanging && !this.trickster.disabled)
-        {
-            this.fireCount();
-        }
-
-        if (this.trickster.disabled)
-        {
-            this.myCollider.radius = 0;
-            this.target = null;
-        }
-
-        if (!this.animator.GetBool("Transition"))
-        {
-            this.isChanging = false;
-        }
-    }
-
-    // update the closest target in range
-    void updateTarget()
-    {
-        if (this.attackMode == "default") this.defaultMode();
     }
 }
--- Groundbreakers/Assets/Scripts/Characters/characterAttributes.cs
+++ Groundbreakers/Assets/Scripts/Characters/characterAttributes.cs
@@ -1,140 +1,90 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class characterAttributes : MonoBehaviour
 {
-    public bool alchemy = false;
-
+    //Stats
+    public int POW = 2;
+    public int ROF = 2;
+    public int RNG = 2;
+    public int MOB = 2;
     public int AMP = 1;
 
-    public bool blightSE = false;
 
-    public bool breakSE = false;
 
-    public bool burnSE = false;
-
-    public bool disabled = false;
-
+    //Attack Effects
+    public bool burstAE = false;
+    public bool ricochetAE = false;
     public bool laserAE = false;
-
-    public bool markSE = false;
-
-    public int MOB = 2;
-
-    // Modules
-    public GameObject[] modules = new GameObject[6];
-
-    public bool netSE = false;
-
-    public bool nullify = false;
-
+    public bool splashAE = false;
     public bool pierceAE = false;
-
-    // Stats
-    public int POW = 2;
-
-    public bool purgeSE = false;
-
+    public bool traceAE = false;
+    public bool cleaveAE = false;
+    public bool whirwindAE = false;
     public bool reachAE = false;
 
-    public bool reaper = false;
-
-    public bool ricochetAE = false;
-
-    public int RNG = 2;
-
-    public int ROF = 2;
-
-    // Status Effects
+    //Status Effects
     public bool slowSE = false;
-
-    public bool splashAE = false;
-
     public bool stunSE = false;
+    public bool burnSE = false;
+    public bool markSE = false;
+    public bool purgeSE = false;
+    public bool breakSE = false;
+    public bool blightSE = false;
+    public bool netSE = false;
+    public bool disabled = false;
 
-    public bool swiftness = false;
-
-    public bool traceAE = false;
-
-    public bool whirwindAE = false;
-
-    // Scripts
+    //Scripts
     characterAttack attack;
 
-    // stance
+    //stance
     private string stance = "gun";
 
-    public void disable()
-    {
-        this.disabled = true;
-    }
+    //Modules
+    public GameObject[] modules = new GameObject[6];
 
-    public void enabled()
+    void Awake()
     {
-        this.disabled = false;
+        attack = GetComponent<characterAttack>();
     }
-
-    // functions
+    //functions
     public void gun()
     {
-        this.stance = "gun";
-        this.POW = 2;
-        this.RNG = 2;
-        this.AMP = 1;
+        stance = "gun";
+        POW = 2;
+        RNG = 2;
+        AMP = 1;
     }
 
     public void melee()
     {
-        this.stance = "melee";
-        this.POW = 3;
-        this.RNG = 1;
-        this.AMP = 2;
+        stance = "melee";
+        POW = 3;
+        RNG = 1;
+        AMP = 2;
     }
 
-    public void resetStats()
+    public void disable()
     {
-        if (this.stance == "gun")
-        {
-            this.POW = 2;
-            this.ROF = 2;
-            this.RNG = 2;
-            this.MOB = 2;
-            this.AMP = 1;
-        }
-        else
-        {
-            this.POW = 3;
-            this.ROF = 2;
-            this.RNG = 1;
-            this.MOB = 2;
-            this.AMP = 2;
-        }
+        disabled = true;
     }
 
-    public void Transform()
+    public void enabled()
     {
-        if (this.stance == "gun")
-        {
-            this.POW += 1;
-            this.RNG -= 1;
-            this.AMP += 1;
-        }
-        else
-        {
-            this.POW -= 1;
-            this.RNG += 1;
-            this.AMP -= 1;
-        }
+        disabled = false;
     }
+
 
     public void updateAttributes(int[] inventory)
     {
-        // reset stats and update character attributes
-        this.resetStats();
-        this.POW += inventory[0];
-        this.ROF += inventory[1];
-        this.RNG += inventory[2];
-        this.MOB += inventory[3];
-        this.AMP += inventory[4];
+        //reset stats and update character attributes
+        resetStats();
+        POW += inventory[0];
+        ROF += inventory[1];
+        RNG += inventory[2];
+        MOB += inventory[3];
+        AMP += inventory[4];
 
         bool[] temp = new bool[17];
         for (int i = 5; i < inventory.Length; i++)
@@ -149,29 +99,59 @@ public class characterAttributes : MonoBehaviour
             }
         }
 
-        // update effects
-        // this.burstAE = temp[0];
-        this.ricochetAE = temp[1];
-        this.laserAE = temp[2];
-        this.splashAE = temp[3];
-        this.pierceAE = temp[4];
-        this.traceAE = temp[5];
-
-        // this.cleaveAE = temp[6];
-        this.whirwindAE = temp[7];
-        this.reachAE = temp[8];
-        this.slowSE = temp[9];
-        this.stunSE = temp[10];
-        this.burnSE = temp[11];
-        this.markSE = temp[12];
-        this.purgeSE = temp[13];
-        this.breakSE = temp[14];
-        this.blightSE = temp[15];
-        this.netSE = temp[16];
+        //update effects
+        burstAE = temp[0];
+        ricochetAE = temp[1];
+        laserAE = temp[2];
+        splashAE = temp[3];
+        pierceAE = temp[4];
+        traceAE = temp[5];
+        cleaveAE = temp[6];
+        whirwindAE = temp[7];
+        reachAE = temp[8];
+        slowSE = temp[9];
+        stunSE = temp[10];
+        burnSE = temp[11];
+        markSE = temp[12];
+        purgeSE = temp[13];
+        breakSE = temp[14];
+        blightSE = temp[15];
+        netSE = temp[16];
     }
 
-    void Awake()
+    public void resetStats()
     {
-        this.attack = this.GetComponent<characterAttack>();
+        if (stance == "gun")
+        {
+            POW = 2;
+            ROF = 2;
+            RNG = 2;
+            MOB = 2;
+            AMP = 1;
+        }
+        else
+        {
+            POW = 3;
+            ROF = 2;
+            RNG = 1;
+            MOB = 2;
+            AMP = 2;
+        }
+    }
+
+    public void Transform()
+    {
+        if (stance == "gun")
+        {
+            POW += 1;
+            RNG -= 1;
+            AMP += 1;
+        }
+        else
+        {
+            POW -= 1;
+            RNG += 1;
+            AMP -= 1;
+        }
     }
 }
