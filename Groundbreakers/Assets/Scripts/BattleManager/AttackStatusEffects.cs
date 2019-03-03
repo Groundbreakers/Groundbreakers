5197e8c645184a65fec1dd9611828e13a1f3f4f0
RaiderSoap <info@chivalrystudio.com>
2019-03-03 11:37:37 -0800

    [rearrange] Update Effects Enum Table

---
+++ Groundbreakers/Assets/Scripts/BattleManager/AttackStatusEffects.cs
@@ -0,0 +1,113 @@
+﻿namespace BattleManager
+{
+    using System;
+
+    /// <summary>
+    /// I assume the attack effects are handled in 'CharacterAttack' Class in the following ways:
+    /// 1. Having a field of type 'AttackEffects' that store attack effects.
+    /// 2. since this is a flag, which means you should use 'AttackEffects.HasFlag() Method'
+    /// to check if one has the desired effects. 
+    /// </summary>
+    /// <see cref="https://docs.microsoft.com/en-us/dotnet/api/system.enum.hasflag?view=netframework-4.7.2"/>
+    [Flags]
+    public enum AttackEffects
+    {
+        /// <summary>
+        /// Release multiple projectiles at once. [Ranged]
+        /// </summary>
+        Burst,
+
+        /// <summary>
+        /// Projectiles bounce to other enemies. [Ranged]
+        /// </summary>
+        Ricochet,
+
+        /// <summary>
+        /// Does laser things. [Ranged]
+        /// </summary>
+        Laser,
+
+        /// <summary>
+        /// Damage all enemies around target. [Ranged]
+        /// </summary>
+        Splash,
+
+        /// <summary>
+        /// Projectiles pierce through enemies and obstacles. [Ranged]
+        /// </summary>
+        Pierce,
+
+        /// <summary>
+        /// Projectiles chase enemies. [Ranged]
+        /// </summary>
+        Trace,
+
+        /// <summary>
+        /// Damage all enemies in front. [Melee]
+        /// </summary>
+        Cleave,
+
+        /// <summary>
+        /// Damage all enemies within range [Melee]
+        /// </summary>
+        Whirlwind,
+
+        /// <summary>
+        /// Melee attacks become ranged. [Melee]
+        /// </summary>
+        Reach,
+    }
+
+    /// <summary>
+    /// I assume these effects are handled within 'EnemyGeneric' class. Do the same as above.
+    /// <see cref="https://docs.microsoft.com/en-us/dotnet/api/system.enum.hasflag?view=netframework-4.7.2"/>
+    /// </summary>
+    [Flags]
+    public enum StatusEffects
+    {
+        /// <summary>
+        /// Half movement speed.
+        /// </summary>
+        Slow,
+
+        /// <summary>
+        /// Cannot move.
+        /// </summary>
+        Stun,
+
+        /// <summary>
+        /// Lose HP over time.
+        /// </summary>
+        Burn,
+
+        /// <summary>
+        /// 25% more damage taken.
+        /// </summary>
+        Mark,
+
+        /// <summary>
+        /// Disable Auras and Revenges
+        /// </summary>
+        Purge,
+
+        /// <summary>
+        /// Disable Armor.
+        /// </summary>
+        Break,
+
+        /// <summary>
+        /// Lose HP over time (stacks).
+        /// </summary>
+        Blight,
+
+        /// <summary>
+        /// 0% chance to dodge.
+        /// </summary>
+        Net,
+
+        /// <summary>
+        /// Slow and 0% chance to dodge. [Scavenger Only]
+        /// </summary>
+        Trap,
+    }
+}
