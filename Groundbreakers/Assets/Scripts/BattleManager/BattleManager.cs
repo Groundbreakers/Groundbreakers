95a1ea93054ac94ea70e0a20ed2a94e1b114564c
RaiderSoap <info@chivalrystudio.com>
2019-03-07 18:25:23 -0800

    meeting changes

--- Groundbreakers/Assets/Scripts/BattleManager/BattleManager.cs
+++ Groundbreakers/Assets/Scripts/BattleManager/BattleManager.cs
@@ -198,7 +198,9 @@
         {
             GameState = Stages.Combating;
 
+
             // Toggle UI
+            Resources.FindObjectsOfTypeAll<GameSpeed>()[0].Toggle();
             GameObject.Find("DeployPanel").GetComponent<Animator>().SetBool("Open", true);
         }
 
@@ -220,9 +222,11 @@
         {
             this.KillAllEnemies();
 
-            // Temp, call the loot 
-            var lootUI = FindObjectOfType<LootGenerator>();
-            lootUI.Toggle();
+            Resources.FindObjectsOfTypeAll<GameSpeed>()[0].Toggle();
+
+            var canvas = GameObject.Find("Canvas");
+            var routes = canvas.GetComponent<RoutesGenerator>();
+            routes.Toggle();
 
             this.timer.ResetTimer();
             GameState = Stages.Null;
