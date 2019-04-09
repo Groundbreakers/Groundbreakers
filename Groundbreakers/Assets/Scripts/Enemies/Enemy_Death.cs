24ea1bea3bd718f4055cd75e9afa04c6b57cc6a3
Javy Wu <javywu1022@gmail.com>
2019-04-08 22:59:18 -0700

    initial; commit

--- Groundbreakers/Assets/Scripts/Enemies/Enemy_Death.cs
+++ Groundbreakers/Assets/Scripts/Enemies/Enemy_Death.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Enemy_Death : MonoBehaviour
 {
     public Animator anim;
+    public GameObject loot;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,14 @@ public class Enemy_Death : MonoBehaviour
     public void setDirection(int i)
     {
         anim.SetInteger("Direction", i);
+        dropLoot();
+    }
+
+    // drop loot when an enemy is killed
+    public void dropLoot() {
+
+        GameObject temp = Instantiate(loot, transform.position, transform.rotation);
+         if(temp != null) Debug.Log("yes");
+         else Debug.Log("no");
     }
 }
