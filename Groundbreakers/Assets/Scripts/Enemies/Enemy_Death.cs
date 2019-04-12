e86968bf7ee670f3f4acbf7cd740adf09e810c3f
Javy Wu <javywu1022@gmail.com>
2019-04-12 09:06:14 -0700

    minor fixes

--- Groundbreakers/Assets/Scripts/Enemies/Enemy_Death.cs
+++ Groundbreakers/Assets/Scripts/Enemies/Enemy_Death.cs
@@ -6,29 +6,16 @@ using UnityEngine;
 public class Enemy_Death : MonoBehaviour
 {
     public Animator anim;
-    // private lootDrop lootDrop;
     public GameObject loot;
  
-    // Start is called before the first frame update
-    void Start()
-    {
-       // lootDrop = this.lootDrop.GetComponent<lootDrop>();
-    }
-
-
     public void setDirection(int i)
     {
         anim.SetInteger("Direction", i);
-        createLoot();
+        createLoot();  // drop loot
     }
 
     public void createLoot()
     {
-
         GameObject temp = (GameObject) Instantiate(loot, transform.position, transform.rotation);
-       
-
     }
-
-
 }
--- Groundbreakers/Assets/lootDrop.cs
+++ Groundbreakers/Assets/lootDrop.cs
@@ -1,106 +1,86 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Numerics;
+﻿using Assets.Scripts;
 using UnityEngine;
-using UnityEngine.UI;
-using Debug = UnityEngine.Debug;
-using Vector2 = UnityEngine.Vector2;
-using Vector3 = UnityEngine.Vector3;
 
 public class lootDrop : MonoBehaviour
 {
-    
-    private float spawnPointY;
-    private float distance ;
-    private bool reachedTop;
+    public int lootValue;
+
     private CrystalCounter CrystalCounter;
+
+    private float distance;
+
     private HP HP;
 
+    private bool reachedTop;
 
+    private float spawnPointY;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        this.spawnPointY = this.transform.position.y;
-        distance = this.transform.position.y - spawnPointY;
-        reachedTop = false;
-        CrystalCounter = GameObject.Find("CrystalCounter").GetComponent<CrystalCounter>();
-        HP = GameObject.Find("HPCounter").GetComponent<HP>();
-
-    }
-
-    // Update is called once per frame
-    void Update() {
-
-        dropLoot();
-        freeLoot();
-
-        int layerMask = (LayerMask.GetMask("loot"));
-    
-       // Debug.Log(layerMask);
-
-        if (Input.GetMouseButtonDown(0))
-        {
-                Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 45));
-                Vector2 mousePos2D = new Vector2(mousePos.x , mousePos.y);
-                RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero, 0f, layerMask);
-               // Debug.DrawRay(mousePos2D, new Vector2(0,1000), Color.red,1000);
-
-          //  if(hit == null) Debug.Log("zz");
-          //  else Debug.Log("jj");
-
-              
-            if (hit.collider != null)
-                {
-               Destroy(hit.collider.gameObject);
-               CrystalCounter.SetCrystals(10000);
-
-
-
-
-            }
-           
-
-
-
-        }
-         
-    }
-
-
-
+    /// <summary>
+    /// PlaceHolder for loot drop animation
+    /// </summary>
     private void dropLoot() {
-
-       
         if (this.reachedTop == false && this.distance <= 0.5f)
         {
             this.transform.Translate(Vector2.up * 2 * Time.deltaTime, Space.World);
-            distance = this.transform.position.y - spawnPointY;
+            this.distance = this.transform.position.y - this.spawnPointY;
 
             if (this.distance >= 0.5f)
             {
                 this.reachedTop = true;
             }
-
-        }else if (this.reachedTop == true && this.distance >= 0f)
+        }
+        else if (this.reachedTop == true && this.distance >= 0f)
         {
             this.transform.Translate(Vector2.down * 2 * Time.deltaTime, Space.World);
-            distance = this.transform.position.y - spawnPointY;
-            
+            this.distance = this.transform.position.y - this.spawnPointY;
         }
-        
-
     }
 
+    /// <summary>
+    /// Free all the loots when GameOver or Exiting stage
+    /// </summary>
     private void freeLoot() {
-        if (this.HP.healthPoint <= 0)
+        if (this.HP.healthPoint <= 0 || BattleManager.GameState == GameStates.Exiting)
         {
-
             Destroy(this.gameObject);
         }
     }
 
+    /// <summary>
+    /// Alternative for built in MouseOnDown()
+    /// </summary>
+    private void OnMouseClick() {
+        int layerMask = (LayerMask.GetMask("loot"));
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos =
+                Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 45));
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero, 0f, layerMask);
 
+            if (hit.collider != null)
+            {
+                Destroy(hit.collider.gameObject);
+                this.CrystalCounter.SetCrystals(this.lootValue);
+            }
+        }
+    }
 
+    // Start is called before the first frame update
+    void Start() {
+        this.CrystalCounter = GameObject.Find("CrystalCounter").GetComponent<CrystalCounter>();
+        this.HP = GameObject.Find("HPCounter").GetComponent<HP>();
+
+        this.spawnPointY = this.transform.position.y;
+        this.distance = this.transform.position.y - this.spawnPointY;
+        this.reachedTop = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        this.OnMouseClick();
+        this.dropLoot();
+        this.freeLoot();
+    }
 }
