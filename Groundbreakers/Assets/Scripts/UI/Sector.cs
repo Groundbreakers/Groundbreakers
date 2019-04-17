f008f839d975efbc9d3695ecc59400b40968bda5
Yuming <lionpkqq@gmail.com>
2019-04-17 05:25:42 -0700

    Simple Sector Map Update

---
+++ Groundbreakers/Assets/Scripts/UI/Sector.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sector : MonoBehaviour
+{
+    public GameObject PathL;
+    public GameObject PathM;
+    public GameObject PathR;
+    public GameObject Button;
+    public string Type;
+    public int Depth;
+    public int Row;
+    public int Column;
+
+    public void Setup(int row, int column, string type, int depth)
+    {
+        this.Row = row;
+        this.Column = column;
+        this.Type = type;
+        this.Depth = depth;
+        this.Button.GetComponent<Button>().onClick.AddListener(this.HandleButton);
+    }
+
+    public void HandleButton()
+    {
+        GameObject.Find("SectorMap").GetComponent<SectorGenerator>().ShowPath(this.Row, this.Column);
+    }
+
+    public void ShowButton()
+    {
+        this.Button.SetActive(true);
+    }
+
+    public void ShowPath(Char path)
+    {
+        this.Button.SetActive(true);
+        if (path == 'L')
+        {
+            this.PathL.SetActive(true);
+        }
+        if (path == 'M')
+        {
+            this.PathM.SetActive(true);
+        }
+        if (path == 'R')
+        {
+            this.PathR.SetActive(true);
+        }
+    }
+}
---
+++ Groundbreakers/Assets/Scripts/UI/SectorGenerator.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEditor;
+
+using UnityEngine;
+
+public class SectorGenerator : MonoBehaviour
+{
+    public GameObject UI;
+    public GameObject Prefab;
+    public GameObject Dummy;
+    public int Height;
+
+    private int heightMultiplier = 120;
+    private int depthMultiplier = 50;
+
+    private int[,] sectorBool;
+    private GameObject[,] sectors;
+
+    void Start()
+    {
+        this.Initialize();
+    }
+
+    public void Initialize()
+    {
+        this.sectorBool = new int[this.Height, 5];
+
+        if (this.Height > 0)
+        {
+            this.sectorBool[0, 1] = this.sectorBool[0, 2] = this.sectorBool[0, 3] = 1;
+        }
+
+        for (int i = 0; i < this.Height - 1; i++)
+        {
+            int sectorCount = this.sectorBool[i, 0] + this.sectorBool[i, 1] + this.sectorBool[i, 2]
+                              + this.sectorBool[i, 3] + this.sectorBool[i, 4];
+            int nextSectorCount = 0;
+            while (nextSectorCount == 0)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (this.sectorBool[i, j] != 0)
+                    {
+                        if (j == 0)
+                        {
+                            if (Random.Range(0, sectorCount) == 0)
+                            {
+                                this.sectorBool[i + 1, 0] = 1;
+                                nextSectorCount++;
+                            }
+
+                            if (Random.Range(0, sectorCount) == 0)
+                            {
+                                this.sectorBool[i + 1, 1] = 1;
+                                nextSectorCount++;
+                            }
+                        }
+                        else if (j == 4)
+                        {
+                            if (Random.Range(0, sectorCount) == 0)
+                            {
+                                this.sectorBool[i + 1, 3] = 1;
+                                nextSectorCount++;
+                            }
+
+                            if (Random.Range(0, sectorCount) == 0)
+                            {
+                                this.sectorBool[i + 1, 4] = 1;
+                                nextSectorCount++;
+                            }
+                        }
+                        else
+                        {
+                            if (Random.Range(0, sectorCount) == 0)
+                            {
+                                this.sectorBool[i + 1, j - 1] = 1;
+                                nextSectorCount++;
+                            }
+
+                            if (Random.Range(0, sectorCount) == 0)
+                            {
+                                this.sectorBool[i + 1, j] = 1;
+                                nextSectorCount++;
+                            }
+
+                            if (Random.Range(0, sectorCount) == 0)
+                            {
+                                this.sectorBool[i + 1, j + 1] = 1;
+                                nextSectorCount++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        this.UI.GetComponent<RectTransform>().sizeDelta = new Vector2(this.UI.GetComponent<RectTransform>().sizeDelta.x, this.heightMultiplier);
+
+        this.sectors = new GameObject[this.Height, 5];
+
+        for (int i = 0; i < this.Height; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                if (this.sectorBool[i, j] == 1)
+                {
+                    this.sectors[i, j] = (GameObject)Instantiate(this.Prefab, this.UI.transform);
+                    this.sectors[i, j].GetComponent<Sector>().Setup(i, j, "Battle", (i + 1) * this.depthMultiplier);
+                    if (i == 0)
+                    {
+                        this.sectors[i, j].GetComponent<Sector>().ShowButton();
+                    }
+                }
+                else
+                {
+                    this.sectors[i, j] = (GameObject)Instantiate(this.Dummy, this.UI.transform);
+                }
+            }
+        }
+    }
+
+    public void ShowPath(int row, int column)
+    {
+        if (row == this.Height - 1)
+        {
+            return;
+        }
+
+        if (this.UI.GetComponent<RectTransform>().sizeDelta.y < this.heightMultiplier * (row + 3))
+        {
+            this.UI.GetComponent<RectTransform>().sizeDelta = new Vector2(this.UI.GetComponent<RectTransform>().sizeDelta.x, this.heightMultiplier * (row + 3));
+        }
+
+        if (column == 0)
+        {
+            if (this.sectors[row + 1, column].GetComponent<Sector>() != null)
+            {
+                this.sectors[row + 1, column].GetComponent<Sector>().ShowPath('M');
+            }
+
+            if (this.sectors[row + 1, column + 1].GetComponent<Sector>() != null)
+            {
+                this.sectors[row + 1, column + 1].GetComponent<Sector>().ShowPath('L');
+            }
+        }
+        else if (column == 4)
+        {
+            if (this.sectors[row + 1, column].GetComponent<Sector>() != null)
+            {
+                this.sectors[row + 1, column].GetComponent<Sector>().ShowPath('M');
+            }
+
+            if (this.sectors[row + 1, column - 1].GetComponent<Sector>() != null)
+            {
+                this.sectors[row + 1, column - 1].GetComponent<Sector>().ShowPath('R');
+            }
+        }
+        else
+        {
+            if (this.sectors[row + 1, column - 1].GetComponent<Sector>() != null)
+            {
+                this.sectors[row + 1, column - 1].GetComponent<Sector>().ShowPath('R');
+            }
+
+            if (this.sectors[row + 1, column].GetComponent<Sector>() != null)
+            {
+                this.sectors[row + 1, column].GetComponent<Sector>().ShowPath('M');
+            }
+
+            if (this.sectors[row + 1, column + 1].GetComponent<Sector>() != null)
+            {
+                this.sectors[row + 1, column + 1].GetComponent<Sector>().ShowPath('L');
+            }
+        }
+            
+    }
+}
