4c10629a3d0d2a217be0abc77e6246e2670eae29
xuyanwen2012 <info@chivalrystudio.com>
2019-05-22 20:53:50 -0700

    mushroom bloooooooooom

---
+++ Groundbreakers/Assets/Scripts/BattleManager/Booooom.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+using Assets.Enemies.Scripts;
+
+using UnityEngine;
+
+public class Booooom : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0.0f, 5.0f)]
+    private float boooomRadius = 2.0f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    private void OnDisable()
+    {
+        var enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        var pos = this.transform.position;
+
+        var targets = enemies.Where(
+            e => Vector3.Distance(pos, e.transform.position) <= this.boooomRadius);
+
+        foreach (var target in targets)
+        {
+            target.GetComponent<Enemy_Generic>().DamageEnemy(999, 999, 1.0f, false, false);
+        }
+    }
+}
