76ce64f8feb7cba90261da541a85589576762fdb
Chris Kong <cxkong@ucsc.edu>
2019-06-06 22:30:20 -0700

    Difficulty

--- Groundbreakers/Assets/Scripts/CombatManager/SetupBattleField.cs
+++ Groundbreakers/Assets/Scripts/CombatManager/SetupBattleField.cs
@@ -35,7 +35,7 @@
 
         [InfoBox("Setup the map and let battle begins.")]
         [Button]
-        public void Setup()
+        public void Setup(int Depth, int Risk)
         {
             this.StartCoroutine(this.Begin());
         }
@@ -63,7 +63,7 @@
         {
             if (Input.GetKeyDown("space"))
             {
-                this.Setup();
+                this.Setup(50, 1);
             }
         }
 
--- Groundbreakers/Assets/Scripts/UI/Difficulty.cs
+++ Groundbreakers/Assets/Scripts/UI/Difficulty.cs
@@ -7,12 +7,13 @@ public class Difficulty : MonoBehaviour
 {
     public Text firstLine;
     public Text secondLine;
+    public int Risk = 1;
 
     // Start is called before the first frame update
     void Start()
     {
         this.firstLine.text = "Risk";
-        this.secondLine.text = "1";
+        this.secondLine.text = "" + Risk;
     }
 
     // Update is called once per frame
